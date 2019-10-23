5b646750acd2160c752b7aadf657cf8d71a0779b
wbg0004 <wbg0004@auburn.edu>
2019-10-22 21:34:06 -0500

    Initial commit

---
+++ src/main/java/LoginServer.java
@@ -0,0 +1,85 @@
+import javax.xml.transform.Result;
+import java.io.PrintWriter;
+import java.net.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class LoginServer {
+    public static void main(String[] args) {
+        HashMap<Integer, String> loginUsers = new HashMap<>();
+
+        int port = 1000;
+        try {
+            ServerSocket server = new ServerSocket(port);
+
+            while (true) {
+                Socket pipe = server.accept();
+                PrintWriter out = new PrintWriter(pipe.getOutputStream(), true);
+                Scanner in = new Scanner(pipe.getInputStream());
+
+                String command = in.nextLine();
+                if (command.equals("LOGIN")) {
+                    String username = in.nextLine();
+                    String password = in.nextLine();
+
+                    System.out.println("Login with username = " + username + " password = " + password);
+
+                    if (checkUser(username, password)) {
+                        int accessToken = username.hashCode();
+                        out.println(accessToken);
+                        loginUsers.put(accessToken, username); // add this user into the Table!
+                    } else {
+                        out.println(0); // access denied
+                        continue;
+                    }
+                }
+
+                if (command.equals("LOGOUT")) {
+                    int token = in.nextInt();
+
+                    if (loginUsers.containsKey(token)) {
+                        System.out.println("Logout with token = " + token);
+                        loginUsers.remove(token); // remove this user
+                        out.println(1); // logout successful!
+                    }
+                    else {
+                        System.out.println("Logout with token = " + token + " failed");
+                        out.println(0); //logout unsuccessful User not logged in
+                    }
+                } else {
+                    out.println(0); // logout unsuccessful!
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean checkUser(String username, String password) {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:" + "/Users/willgarrison/Documents/Software Engineering/Fall 2019/COMP 3700/Activity 13/store.db";
+            conn = DriverManager.getConnection(url);
+
+            String sql = "SELECT * FROM Users WHERE Username = " + "\"" + username + "\""
+                    + " AND  Password = " + "\"" + password + "\"";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            if (rs.next()) {
+                conn.close();
+                return true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}
